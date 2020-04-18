b16ddf27abb6f12960c9701770ef1d5f34dcf126
geetheshtg <tggeethesh@gmail.com>
2020-04-18 20:11:08 +0530

    Minor modifications

--- alert_driver.py
+++ alert_driver.py
@@ -11,15 +11,7 @@ import time
 import dlib
 import cv2
 
-smile = 0
 face_cascade = cv2.CascadeClassifier('/home/geethesh/Documents/imagevenv/lib/python2.7/site-packages/cv2/data/haarcascade_frontalface_default.xml') 
-smile_cascade = cv2.CascadeClassifier('/home/geethesh/Documents/imagevenv/lib/python2.7/site-packages/cv2/data/haarcascade_smile.xml') 
-
-class Smile:
-	smile = 0 
-
-
-sirippu = Smile()
 
 def detect(gray, frame): 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
@@ -27,10 +19,6 @@ def detect(gray, frame):
         cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2) 
         roi_gray = gray[y:y + h, x:x + w] 
         roi_color = frame[y:y + h, x:x + w] 
-        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20) 
-	sirippu.smile = smiles
-        for (sx, sy, sw, sh) in smiles: 
-            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2) 
     return frame
 
 def eye_aspect_ratio(eye):
@@ -52,15 +40,11 @@ EYE_AR_THRESH = 0.25
 EYE_AR_MILLIS = 400
 
 COUNTER = 0
-ALARM_ON = False
 
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
-(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-(lebStart,lebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
-(rebStart,rebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
@@ -82,9 +66,6 @@ while True:
 		shape = predictor(gray, rect)
 		shape = face_utils.shape_to_np(shape)
 
-		mouth = shape[mStart:mEnd]
-		leftEyeBrow = shape[lebStart:lebEnd]
-		rightEyeBrow = shape[rebStart:rebEnd]
 		leftEye = shape[lStart:lEnd]
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)
@@ -95,32 +76,25 @@ while True:
  		stear =str(ear1)
 		
 
-		mouthHull = cv2.convexHull(mouth)
-		leftEyeBrowHull = cv2.convexHull(leftEyeBrow)
-		rightEyeBrowHull = cv2.convexHull(rightEyeBrow)
 		leftEyeHull = cv2.convexHull(leftEye)
 		cv2.drawContours(frame, [leftEyeHull], -1, (150, 0, 150), 1)
 		rightEyeHull = cv2.convexHull(rightEye)
 		cv2.drawContours(frame, [rightEyeHull], -1, (150, 0, 150), 1)
-		print sirippu.smile
-		if sirippu.smile == ():
-			if ear < EYE_AR_THRESH:
-				if  not closeFlag:
-					stopTime = time.time()*1000.0 + EYE_AR_MILLIS
-					closeFlag = True
-			else:
-				stopTime = time.time()*1000.0 +1000
-				closeFlag = False
-				print "DRIVER AWAKE"
-				mixer.music.stop()
+		if ear < EYE_AR_THRESH:
+			if  not closeFlag:
+				stopTime = time.time()*1000.0 + EYE_AR_MILLIS
+				closeFlag = True
+		else:
+			stopTime = time.time()*1000.0 +1000
+			closeFlag = False
+			print "DRIVER AWAKE"
+			mixer.music.stop()
 			
-			if time.time()*1000.0 > stopTime:
-				print "DRIVER FELL ASLEEP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-				mixer.music.play()
-				stopTime = time.time()*1000.0 + 2000
+		if time.time()*1000.0 > stopTime:
+			print "DRIVER FELL ASLEEP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			mixer.music.play()
+			stopTime = time.time()*1000.0 + 2000
 			
-
-
 		cv2.putText(frame, "EAR: {:.2f}".format(ear), (325, 30),
 			cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 0, 0), 2)
 	cv2.imshow("Frame", frame)
@@ -129,9 +103,3 @@ while True:
 		break
 cv2.destroyAllWindows()
 vs.stop()
-
-
-
-
-
-
